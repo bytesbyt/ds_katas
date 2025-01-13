9e727340fd1df8a82af36f3240deabf822b1abae
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-01-13 12:14:08 +0000

    Write a program that simulates rolling a dice.

--- ds_python/dice_rolling_simulator.py
+++ ds_python/dice_rolling_simulator.py
@@ -1,14 +1,33 @@
 ## simulate rolling a dice
-# randomly choose a number bewtween 1 and 6 and print the number
-
-# Asks if you'd like to roll again
-
-# set the min and max number that your dice can produce
 
 import random
-def dice_roll():
-    min = input("What is the min number that dice can produce? Please enter an integer:")
-    max = input("What is the max number that dice can produce? Please enter an integer:")
-    random.randint(min, max)
 
+def dice_roll():
+    while True:
+        try:
+            # set the min and max number that your dice can produce
+            min = int(input("What is the min number that dice can produce? Please enter an integer: "))
+            max = int(input("What is the max number that dice can produce? Please enter an integer: "))
+
+            if min >= max:
+                print("The mininum value must be less than the max value. Please try again.")
+                continue
+            break
+        except ValueError:
+            print("Please enter an integer number.")
+ 
+    
+    while True:
+        # Asks if you'd like to roll again
+        roll = input("Do you want to roll the dice? (y/n): ").lower()
+        if roll == 'y':
+            # randomly choose a number bewtween min and max numbers and print the number
+            print(random.randint(min, max))
+            continue
+        elif roll == 'n':
+            print("Good bye! Thanks for playing.")
+            break
+        else:
+            print("Invalid Input. Please enter 'y' or 'n'.")
+    
 dice_roll()
