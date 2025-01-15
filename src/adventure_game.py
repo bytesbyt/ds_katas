2b33413e6f8afb68b6b8c5f3cf1b28c723918e6b
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-01-15 14:17:42 +0000

    update adventure game

--- src/adventure_game.py
+++ src/adventure_game.py
@@ -1,5 +1,11 @@
-
-
+"""
+A complete text
+game, the program will let users move through rooms based on user input and get descriptions
+of each room. To create this, you'll need to establish the direction in which the user can move, a
+way to track how far the user has moved (and therefore which room he/she is in), and to print
+out a description. You'll also need to set limits for how far the user can move. In other words,
+create “walls” around the rooms that tell the user, “You can't move further in this direction.”
+"""
 
 def adventure_game():
 
@@ -43,7 +49,7 @@ def adventure_game():
         if direction == "left":
             # set limits for how far the user can move
             if current_room_index == 0:
-                print("\nYou cannot go left, try the other direction.")
+                print("\nYou can't move further left, try the other direction.")
             # track which room user has moved to
             else:
                 current_room_index -= 1
@@ -53,7 +59,7 @@ def adventure_game():
         elif direction == "right":
             # set limits for how far the user can move
             if current_room_index == len(room[current_row_index]) -1:
-                print("\nYou cannot go right, try the other direction.")
+                print("\nYou can't move further right, try the other direction.")
             # track which room user has moved to
             else:
                 current_room_index += 1
@@ -63,7 +69,7 @@ def adventure_game():
         elif direction == "up":
             # set limits for how far the user can move
             if current_row_index == 0:
-                print("\nYou cannot go up, try the other direction.")
+                print("\nYou can't move further up, try the other direction.")
             # track which room user has moved to
             else:
                 current_row_index -= 1
@@ -73,7 +79,7 @@ def adventure_game():
         elif direction == "down":
             # set limits for how far the user can move
             if current_row_index == len(room) -1:
-                print("\nYou cannot go down, try the other direction.")
+                print("\nYou can't move further down, try the other direction.")
             # track which room user has moved to
             else:
                 current_row_index += 1
@@ -81,6 +87,7 @@ def adventure_game():
                 print(f"\nYou moved down to the {room[current_row_index][current_room_index]}.")
         
         elif direction == "stop":
+            # exits the program
             print(f"\nYou are in the {room[current_row_index][current_room_index]}. Thank you for your visit!")
             break
 
