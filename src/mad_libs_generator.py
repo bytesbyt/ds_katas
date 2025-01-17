a4f0a1932c6c5463a373cdbff6000f9e29f20420
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-01-17 23:07:39 +0000

    Update mad libs generator

--- src/mad_libs_generator.py
+++ src/mad_libs_generator.py
@@ -6,7 +6,31 @@ premade story template. You'll need prompts for user input, and to then print ou
 the end with the input included.
 """
 
-def mad_libs_generator():
+def generate_story(responses):
+    """
+    Utility function to generate a Mad Libs story from user input
+    """
+
+    # Assign the responses to the meaningful variable names
+    adjective, noun, country, building, prize_1, prize_2, emotion, verb, famous_person, animal = responses
+
+    # Pre-made story template with user input
+    story = f"""
+
+    Your Mad Libs story:
+
+    Once upon a time there was a {adjective} {noun}. One day, the {noun} went to {country} and discovered a hidden {building} with one {prize_1} and one {prize_2}.
+    Upon seeing the {prize_1} and the {prize_2}, the {noun} felt {emotion} and started {verb}.
+    {famous_person} then walked in and saw the {noun} {verb}, and told the {noun} that if they decided to stay in the {building}, they could keep the {prize_1} and the {prize_2} but would need to fight against a {animal}.
+    If the {noun} decided to leave then they would go back to {country} and never see a {prize_2} again.
+    Since the {noun} didn't want to fight against a {animal}, the {noun} decided to give up the {prize_2} and return to {country} with a {prize_1}.
+
+    """
+    return story
+
+
+
+def main():
 
     # Define a list of prompts for user input
     prompts = [
@@ -23,7 +47,7 @@ def mad_libs_generator():
     ]
 
     # Append user input for each prompt to the list
-    response = []
+    responses = []
     # Prompt_index will be increased by 1 each time user responds
     prompt_index = 0
 
@@ -34,30 +58,17 @@ def mad_libs_generator():
             user_input = input(prompts[prompt_index]).strip()
             
             if user_input:
-                response.append(user_input)
+                responses.append(user_input)
                 prompt_index += 1
                 break
             else:
                 print("Input cannot be blank. Please try again.")
 
-    # Assign the responses to the meaningful variable names
-    adjective, noun, country, building, prize_1, prize_2, emotion, verb, famous_person, animal = response
-
-    # Pre-made story template with user input
-    story = f"""
-
-    Your Mad Libs story:
-
-    Once upon a time there was a {adjective} {noun}. One day, the {noun} went to {country} and discovered a hidden {building} with one {prize_1} and one {prize_2}.
-    Upon seeing the {prize_1} and the {prize_2}, the {noun} felt {emotion} and started {verb}.
-    {famous_person} then walked in and saw the {noun} {verb}, and told the {noun} that if they decided to stay in the {building}, they could keep the {prize_1} and the {prize_2} but would need to fight against a {animal}.
-    If the {noun} decided to leave then they would go back to {country} and never see a {prize_2} again.
-    Since the {noun} didn't want to fight against a {animal}, the {noun} decided to give up the {prize_2} and return to {country} with a {prize_1}.
-
-    """
-
-    # print out the full story at the end with the input included
+    # Generate the story
+    story= generate_story(responses)
+    
+    # print full story
     print(story)
 
-# Run the Mad Libs generator
-mad_libs_generator()
+if __name__ == "__main__":
+    main()
